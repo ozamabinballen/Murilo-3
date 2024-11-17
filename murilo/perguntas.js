312562cb086bf28bc6adb4bdcef5b54c0e602eea
Vladimir_O_Cutucador <146009567+ozamabinballen@users.noreply.github.com>
2024-11-17 16:59:08 -0300

    Add files via upload
    ahhh Guilherme

---
+++ murilo/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+'League Of Legends',
+'Quem é o rei dos ladrões?',
+'Gangplank'
+)
+
+criaCartao(
+'futebol',
+'Qual é o maior time do Brasil?',
+'Gremio'
+)
+
+criaCartao(
+'Guilherme',
+'Qual a turma favorita do Guilherme?',
+'2 B'
+)
+
+criaCartao(
+'basquete',
+'quem é o maior jogador de todos os tempos?',
+'Michael Jordan'
+)
